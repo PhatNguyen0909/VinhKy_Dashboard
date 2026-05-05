f00eb2d1494d4511b6b0abb13cbd3a6f1758c71b
PhatNguyen0909 <nhiknh112233@gmail.com>
2026-05-05 09:55:27 +0700

    fix bug

--- dashboard-restauurant/src/components/SideBar/Sidebar.jsx
+++ dashboard-restauurant/src/components/SideBar/Sidebar.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Sidebar.css';
+import './SideBar.css';
 
 // Inline SVG icons (no extra assets needed)
 const IconAdd = (props) => (
